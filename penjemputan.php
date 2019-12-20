1bd10f5fb0567157d638972090471e89ad2a6599
desywahyuningtyas <58194360+desywahyuningtyas@users.noreply.github.com>
2019-12-20 10:17:09 +0700

    tampilan google maps
    Yang awalnya tampilan google maps terlalu besar, sudah diperkecil. Bagian search dan button edit alamat sudah ada dibagian kanan dari google maps

---
+++ penjemputan.php
@@ -0,0 +1,294 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style_riwayat.css">
+    <title>HI Trash - Profil</title>
+  </head>
+  <body style="color: grey;">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">PENJEMPUTAN</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link active" href="#">Beranda<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Edukasi</a>
+            <a class="nav-item nav-link" href="#">Tentang</a>  
+            <a class="nav-item nav-link btn btn-primary " href="Profil.html">Akun Saya</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+<div> <center>Alamat</center></div><html lang="en"> 
+   <head>
+      <meta charset="utf-8">
+      <title>Judul Halaman Saya</title>
+    <style type="text/css">
+    form {
+      width:500px;
+      margin:50px auto;
+    }
+    .search {
+      float: right;
+      padding:8px 15px;
+      background:rgba(50, 50, 50, 0.2);
+      border:0px solid #dbdbdb;
+    }
+    .button {
+      float: right;
+      position:relative;
+      padding:6px 15px;
+      left:-8px;
+      border:2px solid #53bd84;
+      background-color:#53bd84;
+      color:#fafafa;
+    }
+    .button:hover  {
+      background-color:#fafafa;
+      color:#207cca;
+    }.btn{
+      margin-right: 30px;
+      margin-left: 800px;
+      margin-top: -32px;
+  }
+    /**/
+    ::-webkit-input-placeholder { /* For WebKit browsers */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-moz-placeholder { /* For Mozilla Firefox 4 to 18 */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    ::-moz-placeholder { /* For Mozilla Firefox 19+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-ms-input-placeholder { /* For Internet Explorer 10+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }   
+    .row{
+      margin-top: 20px;
+      }
+  </style>
+   </head>
+<body>
+  <div class="container">
+
+  <div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-3"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15798.074398795034!2d113.7275701!3d-8.1503887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6950098a2b04b%3A0x8a34dd2bb51c1741!2sDinas%20Kesehatan%20Kabupaten%20Jember!5e0!3m2!1sen!2sid!4v1576558458539!5m2!1sen!2sid" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </div>
+    
+    <div class="col-md-4 col-sm-8">
+        <input class="button" type="button" value="Cari">
+    <input class="search" type="text" placeholder="Cari..." required>
+   
+      </div>
+     <div class="btn">
+       <div class="btn-group">
+        <button type="button" class="btn btn-success">Edit Alamat</button>
+      </div>
+     </div>
+    </div>
+  </div>
+  </div>
+</div>
+<html>
+<head>
+  <title>Button</title>
+  <style>
+    .btn_style{
+      border: 1px solid #cecece;
+      border-radius: 3px;
+      padding: 3px 10px;
+      box-shadow: 2px 2px 6px rgba (0, 0, 0, 0.4);
+      color: white;
+      background-color: green;
+    }
+    .btn_style:hover{
+      border: 1px solid #b1b1b1;
+      box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+
+    }
+  </style>
+</head>
+<body>
+
+ <!--  <right><button class="btn_style">Edit Alamat</button></right> -->
+</body>
+</html>
+<body>
+
+<div><center>Jenis Sampah</center></div><html lang="en"> 
+   <head>
+      <meta charset="utf-8">
+      <title>Judul Halaman</title>
+    <style type="text/css">
+    form {
+      width:500px;
+      margin:50px auto;
+    }
+    .search {
+      padding:8px 15px;
+      background:rgba(50, 50, 50, 0.2);
+      border:0px solid #dbdbdb;
+    }
+    .button {
+      position:relative;
+      padding:6px 15px;
+      left:-8px;
+      border:2px solid #53bd84;
+      background-color:#53bd84;
+      color:#fafafa;
+    }
+    .button:hover  {
+      background-color:#fafafa;
+      color:#207cca;
+    }
+          
+    ::-webkit-input-placeholder { /* For WebKit browsers */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-moz-placeholder { /* For Mozilla Firefox 4 to 18 */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    ::-moz-placeholder { /* For Mozilla Firefox 19+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-ms-input-placeholder { /* For Internet Explorer 10+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }   
+  </style>
+   </head>
+<body>
+  <div class="container">
+        <h2> Pilihan Jenis Sampah</h2>
+        <form>
+          <div class="form-group">
+            <label for ="se"> Organik</label>
+            <select class="form-control" id="shell">
+          <option>Makanan</option>
+          <option>Minyak</option>
+          </select>
+
+        </div>
+        
+          <div class="form-group">
+          <label for ="se"> Non Organik</label>
+            <select class="form-control" id="shell">
+          <option>Plastik</option>
+          <option>Kardus</option>
+          <option>Kertas</option>
+          <option>Kaleng</option>
+          <option>Kain</option>
+
+        </select>
+      </div>
+</form>
+</body>
+      <div><center>Jadwal Penjemputan</center></div><html lang="en"> 
+   <head>
+      <meta charset="utf-8">
+      <title>Judul</title>
+    <style type="text/css">
+    form {
+      width:500px;
+      margin:50px auto;
+    }
+    .search {
+      padding:8px 15px;
+      background:rgba(50, 50, 50, 0.2);
+      border:0px solid #dbdbdb;
+    }
+    .button {
+      position:relative;
+      padding:6px 15px;
+      left:-8px;
+      border:2px solid #53bd84;
+      background-color:#53bd84;
+      color:#fafafa;
+    }
+    .button:hover  {
+      background-color:#fafafa;
+      color:#207cca;
+    }
+          
+    ::-webkit-input-placeholder { /* For WebKit browsers */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-moz-placeholder { /* For Mozilla Firefox 4 to 18 */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    ::-moz-placeholder { /* For Mozilla Firefox 19+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }
+    :-ms-input-placeholder { /* For Internet Explorer 10+ */
+      color:    #dd3333;
+            font-family:Helvetica Neue;
+            font-weight:bold;
+    }   
+  </style>
+   </head>
+   <html>
+
+<head>
+
+</head>
+
+<body>
+  <label for="inputlg">Tanggal/Bulan/Tahun</label>
+  <input class="form-control input-sm" type="date" name="tanggal">
+     </body>
+
+</html>
+      <label>Waktu  :</label>
+      <input type="text" name="Waktu" placeholder="waktu"/>
+    </p>
+    <p>
+    <center><button class="btn_style">Selesai</button></center>
+    </p>
+    </fieldset>
+  </form> 
+</body>
+</html>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
